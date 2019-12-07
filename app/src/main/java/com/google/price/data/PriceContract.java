0d9fff1e4bfd712e777d43e3c181ea0e0da51240
De-Gald <54669029+De-Gald@users.noreply.github.com>
2019-12-08 02:03:07 +0800

    Update database

--- app/src/main/java/com/google/price/data/PriceContract.java
+++ app/src/main/java/com/google/price/data/PriceContract.java
@@ -7,7 +7,9 @@ public class PriceContract {
     public static final class PriceEntry implements BaseColumns {
         public static final String TABLE_NAME = "priceData";
         public static final String COLUMN_VALUE = "item_price";
-        public static final String COLUMN_LINK = "link";
+        public static final String COLUMN_TITLE = "item_title";
+        public static final String COLUMN_LINK_TO_PAGE = "link_to_page";
+        public static final String COLUMN_LINK_TO_ICON = "link_to_icon";
         public static final String COLUMN_TIMESTAMP = "timestamp";
     }
 
--- app/src/main/java/com/google/price/data/PriceDbHelper.java
+++ app/src/main/java/com/google/price/data/PriceDbHelper.java
@@ -10,7 +10,7 @@ public class PriceDbHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "price.db";
 
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     public PriceDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,7 +22,9 @@ public class PriceDbHelper extends SQLiteOpenHelper {
         final String SQL_CREATE_PRICE_TABLE = "CREATE TABLE " + PriceContract.PriceEntry.TABLE_NAME + " (" +
                 PriceContract.PriceEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                 PriceContract.PriceEntry.COLUMN_VALUE + " STRING NOT NULL, " +
-                PriceContract.PriceEntry.COLUMN_LINK + " STRING NOT NULL, " +
+                PriceContract.PriceEntry.COLUMN_TITLE + " STRING NOT NULL, " +
+                PriceContract.PriceEntry.COLUMN_LINK_TO_PAGE + " STRING NOT NULL, " +
+                PriceContract.PriceEntry.COLUMN_LINK_TO_ICON + " STRING NOT NULL, " +
                 PriceContract.PriceEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
                 "); ";
 
